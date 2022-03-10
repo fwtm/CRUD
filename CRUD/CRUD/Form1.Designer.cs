bb341b4c3d741d97977ad6e776f7bc387bfc41a7
fwtm <fabricio.toapanta@gmail.com>
2022-03-09 22:59:15 -0500

    Add files via upload

---
+++ CRUD/CRUD/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.dgbData = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cbProvincias = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Location = new System.Drawing.Point(12, 12);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(151, 23);
+            this.btnShowData.TabIndex = 1;
+            this.btnShowData.Text = "Show Data";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            this.btnShowData.Click += new System.EventHandler(this.btnShowData_Click);
+            // 
+            // dgbData
+            // 
+            this.dgbData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbData.Location = new System.Drawing.Point(294, 248);
+            this.dgbData.Name = "dgbData";
+            this.dgbData.RowHeadersWidth = 51;
+            this.dgbData.RowTemplate.Height = 24;
+            this.dgbData.ShowCellToolTips = false;
+            this.dgbData.Size = new System.Drawing.Size(399, 176);
+            this.dgbData.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(758, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(758, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Actualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(758, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // cbProvincias
+            // 
+            this.cbProvincias.FormattingEnabled = true;
+            this.cbProvincias.Location = new System.Drawing.Point(12, 113);
+            this.cbProvincias.Name = "cbProvincias";
+            this.cbProvincias.Size = new System.Drawing.Size(121, 24);
+            this.cbProvincias.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(890, 436);
+            this.Controls.Add(this.cbProvincias);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgbData);
+            this.Controls.Add(this.btnShowData);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgbData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.DataGridView dgbData;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox cbProvincias;
+    }
+}
+
---
+++ CRUD/CRUD/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SqlConnectionLibrary;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnectionLibrary.SqlConnections con = new SqlConnections();
+        public Form1()
+        {
+            InitializeComponent();
+            LlenarCombo(cbProvincias);
+        }
+
+        private void btnShowData_Click(object sender, EventArgs e)
+        {
+            dgbData.DataSource = con.SelectDataTableByQuery("select * from Ciudad");
+        }
+
+        private void LlenarCombo(ComboBox cb)
+        {
+           cb.DisplayMember = "Nombre";
+           cb.ValueMember = "Id_Pro";
+           cb.DataSource = con.SelectDataTableByQuery("select Id_Pro, Nombre from Provincia order by 2 asc");
+        }
+    }
+}
